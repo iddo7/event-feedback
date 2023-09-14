14ac4589a1ae62ad948e9301d7cdb00c00b4bb69
iddo <61661956+iddo7@users.noreply.github.com>
2023-09-14 16:55:27 -0400

    Implementing basic design

---
+++ php/events.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Events</title>
+</head>
+<body>
+    <div class="container">
+        <div class="event-card card">
+            <div class="row">
+                <div class="col-3">
+                    <img class="event-card-img img-fluid" src="https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/6470125/910/607/m2/fpnw/wm1/i80hbla2s6wsmd6jaaoeipipz7uvnmxydfidhnsvkwdivvi92u99nvchp3dq19ey-.jpg?1559306650&s=afd67a41ad3ac0ba22618abafc71e535">
+                </div>
+                <div class="col-9 d-flex align-items-center">
+                    <div>
+                        <h3 class="event-card-title">Party Pizza</h3>
+                        <ul class="event-card-list">
+                            <li class="event-card-item">18 novembre</li>
+                            <li class="event-card-item">Informatique</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="event-card card">
+            <div class="row">
+                <div class="col-3">
+                    <img class="event-card-img img-fluid" src="https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/6470125/910/607/m2/fpnw/wm1/i80hbla2s6wsmd6jaaoeipipz7uvnmxydfidhnsvkwdivvi92u99nvchp3dq19ey-.jpg?1559306650&s=afd67a41ad3ac0ba22618abafc71e535">
+                </div>
+                <div class="col-9 d-flex align-items-center">
+                    <div>
+                        <h3 class="event-card-title">Party Pizza</h3>
+                        <ul class="event-card-list">
+                            <li class="event-card-item">18 novembre</li>
+                            <li class="event-card-item">Informatique</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
