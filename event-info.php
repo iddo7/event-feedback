27b5446ea0d23fa7692040ff4c9afd16b2ea65e4
Julien <julienfortin05@gmail.com>
2023-09-14 17:19:43 -0400

    card layout

--- event-info.php
+++ event-info.php
@@ -10,13 +10,28 @@
 </head>
 <body>
 
-<div class="container-fluid">
-    <div class="row custom-margin">
-        <div class="img-container">
-            <img src="https://riotfest.org/wp-content/uploads/2016/10/151_1stuffed_crust_pizza.jpg" alt="Image de pizza">
+<div class="container-fluid vh-100">
+    <div class="row height100">
+        <div class="col-md-6 height100">
+            <div class="card border-0 height100">
+                <img class="card-img-top height100" src="https://riotfest.org/wp-content/uploads/2016/10/151_1stuffed_crust_pizza.jpg" alt="Card image cap">
+            </div>
+        </div>
+        <div class="col-md-6 height100">
+            <div class="card border-0 height100">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+
+
+
 
 
 
