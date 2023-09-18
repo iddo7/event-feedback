f8f40d8942450a1ef38bf7657bef5ea8f446ecd6
julien_le_gros <julienfortin05@gmail.com>
2023-09-18 13:15:30 -0400

    final layout event-info

--- event-info.php
+++ event-info.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
     <title>event-info</title>
@@ -17,27 +18,35 @@
                 <img class="card-img-top height100" src="https://riotfest.org/wp-content/uploads/2016/10/151_1stuffed_crust_pizza.jpg" alt="Card image cap">
             </div>
         </div>
-        <div class="col-md-6 height100">
-            <div class="card border-0 height100">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div class="col-md-6 height100 d-flex align-items-center">
+            <div class="card border-0 mx-auto">
+                <div class="card-body text-center">
+                    <h5 class="card-title text-center display-3 mb-5">Card title</h5>
+                    <p class="card-text m-5" style="font-size: 2rem;">Date: </p>
+                    <p class="card-text m-5" style="font-size: 2rem;">Infos: </p>
+                    <div class="row">
+                        <div class="col-md-4 cercle">
+                            <i class="fa-solid fa-circle" style="color: #59eb24;"></i>
+                            <h2 class="display-5">1</h2>                                              
+                        </div>
+                        <div class="col-md-4 cercle">
+                            <i class="fa-solid fa-circle" style="color: #59eb24;"></i>
+                            <h2 class="display-5">2</h2>                                              
+                        </div>
+                        <div class="col-md-4 cercle">
+                            <i class="fa-solid fa-circle" style="color: #59eb24;"></i>  
+                            <h2 class="display-5">3</h2>                      
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center m-5">
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-
-
-
-
-
-
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
