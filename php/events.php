087cc83300cf8ceef4fd062200148deedeb0b653
iddo07 <isaacnegreiros10@gmail.com>
2023-09-18 12:32:53 -0400

    Linking to DB

--- php/events.php
+++ php/events.php
@@ -8,40 +8,54 @@
     <link rel="stylesheet" href="../style/style.css">
     <title>Events</title>
 </head>
+<?php 
+// if ($_SESSION["connexion"] == true) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $db = "event_feedback";
+
+    // Create connection
+    $connection = new mysqli($servername, $username, $password, $db);
+
+    // Check connection
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+
+    $connection->query('SET NAMES utf8');
+
+    $selectAllQuery = "SELECT * FROM events";
+    $result = $connection->query($selectAllQuery);
+    if ($result->num_rows <= 0) {
+        echo "0 results";
+    }
+?>
 <body>
-    <div class="container">
-        <div class="event-card card">
-            <div class="row">
-                <div class="col-3">
-                    <img class="event-card-img img-fluid" src="https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/6470125/910/607/m2/fpnw/wm1/i80hbla2s6wsmd6jaaoeipipz7uvnmxydfidhnsvkwdivvi92u99nvchp3dq19ey-.jpg?1559306650&s=afd67a41ad3ac0ba22618abafc71e535">
-                </div>
-                <div class="col-9 d-flex align-items-center">
-                    <div>
-                        <h3 class="event-card-title">Party Pizza</h3>
-                        <ul class="event-card-list">
-                            <li class="event-card-item">18 novembre</li>
-                            <li class="event-card-item">Informatique</li>
-                        </ul>
+    <div class="container">  
+        <?php 
+            while($row = $result->fetch_assoc()) {
+                ?>
+                <div class="event-card card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img class="event-card-img img-fluid" src="<?php echo $row["img"] ?>">
+                        </div>
+                        <div class="col-9 d-flex align-items-center">
+                            <div>
+                                <h3 class="event-card-title"><?php echo $row["name"] ?></h3>
+                                <ul class="event-card-list">
+                                    <li class="event-card-item"><?php echo $row["dateStart"] ?></li>
+                                    <li class="event-card-item"><?php echo $row["departementId"] ?></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="event-card card">
-            <div class="row">
-                <div class="col-3">
-                    <img class="event-card-img img-fluid" src="https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/6470125/910/607/m2/fpnw/wm1/i80hbla2s6wsmd6jaaoeipipz7uvnmxydfidhnsvkwdivvi92u99nvchp3dq19ey-.jpg?1559306650&s=afd67a41ad3ac0ba22618abafc71e535">
-                </div>
-                <div class="col-9 d-flex align-items-center">
-                    <div>
-                        <h3 class="event-card-title">Party Pizza</h3>
-                        <ul class="event-card-list">
-                            <li class="event-card-item">18 novembre</li>
-                            <li class="event-card-item">Informatique</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+                <?php
+            }
+        ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
