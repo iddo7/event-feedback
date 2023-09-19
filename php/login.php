5e4bf15a67e90996406e689b7aaa8dea7eeee344
iddo <isaacnegreiros10@gmail.com>
2023-09-18 20:18:36 -0400

    Resizing & Globalizing style

--- php/login.php
+++ php/login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/style.css">
-    <title>Login</title>
+    <title>Event Feedback</title>
 </head>
 <?php 
     $valuesInputed = array(
@@ -96,10 +96,16 @@
     }
 ?>
 <body>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12 text-center">
+                <span class="logo">Event Feedback</span>
+            </div>
+        </div>
+    </div>
     <div class="card p-4 screen-center col-md-3">
-        <h4 class="card-title text-center">Event Feedback</h4>
+        <h1 class="text-center">Connexion</h1>
         <hr>
-        <h5 class="text-center"><span>Connexion</span></h5>
 
         <?php 
             if ($_SERVER['REQUEST_METHOD'] != 'POST' || $errorOccured == true) {
--- php/test-style.php
+++ php/test-style.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
-    <title>Test Style</title>
+    <title>Event Feedback</title>
 </head>
 <body>
     <div class="container-fluid">
