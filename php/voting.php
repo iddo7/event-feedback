39c7e13435c04e58c5a97838346559a646a7b745
iddo <61661956+iddo7@users.noreply.github.com>
2023-09-28 16:28:10 -0400

    Working voting no visual feedback

--- php/voting.php
+++ php/voting.php
@@ -14,22 +14,34 @@ session_start();
     <title>event-info</title>
 </head>
 <body>
-
 <?php 
 if ($_SESSION["connexion"] == true) {
 
+    if (!isset($_GET["id"]) || $_GET["id"] == null || !isset($_GET["type"]) || $_GET["type"] == null) {
+        header("Location: ../index.php");
+        exit;
+    }
+
+    $eventId = $_GET["id"];
+    $voteType = $_GET["type"];
 ?>
 
 <div class="container-fluid screen-center-y">
     <div class="row text-center cercle-x">
         <div class="col-sm-4 col-md-4">
-            <i class="fa-solid fa-circle img-fluid clickable-circle" style="color: #008a64;"></i>
+            <a href="increment-vote.php?type=<?php echo $voteType ?>&id=<?php echo $eventId ?>&feedback=green">
+                <i class="fa-solid fa-circle text-success"></i>
+            </a>
         </div>
         <div class="col-sm-4 col-md-4">
-            <i class="fa-solid fa-circle clickable-circle" style="color: #ffc45d;"></i>
+            <a href="increment-vote.php?type=<?php echo $voteType ?>&id=<?php echo $eventId ?>&feedback=yellow">
+                <i class="fa-solid fa-circle text-warning"></i>
+            </a>
         </div>
         <div class="col-sm-4 col-md-4">
-            <i class="fa-solid fa-circle clickable-circle" style="color: #df2350;"></i>
+            <a href="increment-vote.php?type=<?php echo $voteType ?>&id=<?php echo $eventId ?>&feedback=red">
+                <i class="fa-solid fa-circle text-danger"></i>
+            </a>
         </div>
     </div>
 </div>
@@ -40,17 +52,6 @@ if ($_SESSION["connexion"] == true) {
 <div class="fixed-bottom text-center mb-4">
     <span class="">vote étudiant</span>
 </div>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('.clickable-circle').click(function() {
-            // Handle the click event here
-            alert('Circle clicked!');
-            // You can replace the alert with any action you want to perform
-        });
-    });
-</script>
 
 <?php 
 }
