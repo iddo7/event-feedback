e0a455e2a13ca98ed0a9126feb189cc8988ea12b
iddo <61661956+iddo7@users.noreply.github.com>
2023-10-05 15:05:11 -0400

    Fixing modify event error

--- php/modify-event.php
+++ php/modify-event.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include 'navbar.php';
 ?>
+<?php include 'navbar.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,42 +13,47 @@ include 'navbar.php';
     <link rel="stylesheet" href="../style/style.css">
     <title>Event Feedback</title>
 </head>
-
 <?php 
 
 function anyIsEmpty($arrayOfInputs) {
     $result = false;
     foreach ($arrayOfInputs as $input) {
-        if ($input !== "mdp" && empty($_POST[$input])) {
+        if (empty($_POST[$input])) {
             $result = true;
             break;
         }
     }
+
     return $result;
 }
 
 function trojan($data){
-    $data = trim($data);
-    $data = addslashes($data);
-    $data = htmlspecialchars($data);
+    $data = trim($data); //Enleve les caractères invisibles
+    $data = addslashes($data); //Mets des backslashs devant les ' et les  "
+    $data = htmlspecialchars($data); // Remplace les caractères spéciaux par leurs symboles comme ­< devient &lt;
+    
     return $data;
 }
 
 if ($_SESSION["connexion"] == true) {
 
     $valuesInputed = array(
-        "prenom" => "", 
-        "email" => "",
-        "mdp" => "",
+        "name" => "",
+        "date" => "",
+        "description" => "",
+        "img" => "",
+        "departementId" => "",
+        "place" => "", // Ajout de la valeur "place"
     );
     $errorOccured = false;
     $alertMessage = '';
 
     $eventId = isset($_GET["id"]) ? $_GET["id"] : $_POST["hiddenId"];
 
+    // FORM WAS SUBMITTED
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $inputs = array("prenom", "email", "mdp");
+        $inputs = array("name", "date", "description", "img", "departementId", "place"); // Ajout de "place" ici
 
         if (anyIsEmpty($inputs)) {
             $errorOccured = true;
@@ -60,37 +65,34 @@ if ($_SESSION["connexion"] == true) {
             $valuesInputed[$keys[$i]] = trojan($_POST[$inputs[$i]]);
         }
 
-        // Only update the password if it's provided in the form
-        if (!empty($_POST['mdp'])) {
-            $valuesInputed['mdp'] = md5($_POST['mdp']);
-        }
-
         if (!$errorOccured) {
             $servername = "localhost";
             $username = "root";
             $password = "root";
             $db = "event_feedback";
         
+            // Create connection
             $connection = mysqli_connect($servername, $username, $password, $db);
         
+            // Check connection
             if (!$connection) {
                 die("Connection failed: " . mysqli_connect_error());
             }
             $connection->query('SET NAMES utf8');
 
-            $prenom = $valuesInputed['prenom'];
-            $email = $valuesInputed['email'];
-            $mdp = $valuesInputed['mdp'];
-            
-            $updateQuery = "UPDATE users SET prenom='{$prenom}', email='{$email}'";
-            if (!empty($_POST['mdp'])) {
-                $updateQuery .= ", password='{$mdp}'";
-            }
-            $updateQuery .= " WHERE id={$eventId}";
-            
+            $updatedEvent_name = $valuesInputed['name'];
+            $updatedEvent_description = $valuesInputed['description'];
+            $updatedEvent_date = $valuesInputed['date'];
+            $updatedEvent_img = $valuesInputed['img'];
+            $updatedEvent_departementId = $valuesInputed['departementId'];
+            $updatedEvent_place = $valuesInputed['place']; // Récupération de la valeur de "place"
+            $updateQuery = "UPDATE events SET name='{$updatedEvent_name}', description=\"{$updatedEvent_description}\", date='{$updatedEvent_date}', img='{$updatedEvent_img}', departementId={$updatedEvent_departementId}, place='{$updatedEvent_place}' WHERE id={$eventId}"; // Ajout de "place" dans la requête SQL
+
+            echo $updateQuery;
+
             if ($connection->query($updateQuery) === TRUE) {
                 $alertMessage = "L'ajout s'est bien produit";
-                header("Location: users.php");
+                header("Location: events.php");
                 exit;
             }
             else {
@@ -101,11 +103,12 @@ if ($_SESSION["connexion"] == true) {
             mysqli_close($connection);
         }
     }
-?>
 
+    
+?>
 <body>
     <div class="p-4 screen-center col-12 col-md-6 col-xl-4">
-        <h1 class="text-center">Modifier utilisateur</h1>
+        <h1 class="text-center">Modifier évènement</h1>
         <hr>
 
         <?php 
@@ -116,14 +119,17 @@ if ($_SESSION["connexion"] == true) {
                 $password = "root";
                 $db = "event_feedback";
             
+                // Create connection
                 $connection = new mysqli($servername, $username, $password, $db);
             
+                // Check connection
                 if ($connection->connect_error) {
                     die("Connection failed: " . $connection->connect_error);
                 }
+                $connection->query('SET NAMES utf8');
             
                 $eventId = isset($_GET["id"]) ? $_GET["id"] : $_POST["hiddenId"];
-                $selectAllQuery = "SELECT * FROM users WHERE id=" . $eventId;
+                $selectAllQuery = "SELECT * FROM events WHERE id=" . $eventId;
                 $result = $connection->query($selectAllQuery);
                 if ($result->num_rows <= 0) {
                     echo "0 results";
@@ -131,9 +137,12 @@ if ($_SESSION["connexion"] == true) {
             
                 while($row = $result->fetch_assoc()) {
                     $valuesInputed = array(
-                        "prenom" => $row["prenom"],
-                        "email" => $row["email"],
-                        "password" => $row["password"],
+                        "name" => $row["name"],
+                        "description" => $row["description"],
+                        "date" => $row["date"],
+                        "img" => $row["img"],
+                        "departementId" => $row["departementId"],
+                        "place" => $row["place"], // Récupération de la valeur de "place"
                     );
                 }
                 $errorOccured = false;
@@ -141,13 +150,59 @@ if ($_SESSION["connexion"] == true) {
         ?>
                 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post" class="">
 
-                    <input type="text" class="form-control mb-3" name="prenom" id="prenom" placeholder="Prénom" 
-                    value="<?php echo $valuesInputed['prenom']; ?>">
+                    <div class="row">
+                        <div class="col-6">
+                            <input type="text" class="form-control mb-3" name="name" id="name" placeholder="Nom de l'évènement" value="<?php echo $valuesInputed['name']; ?>">
+                        </div>
+                        <div class="col-6">
+                            <input type="date" class="form-control mb-3" name="date" id="date" value="<?php echo $valuesInputed['date']; ?>">
+                        </div>
+                    </div>
 
-                    <input type="text" class="form-control mb-3" name="email" id="email" placeholder="email" 
-                    value="<?php echo $valuesInputed['email']; ?>">
+                    <input type="text" class="form-control mb-3" name="place" id="lien" placeholder="place" 
+                    value="<?php echo $valuesInputed['place']; ?>">
 
-                    <input type="password" class="form-control mb-3" name="mdp" id="mdp" placeholder="Nouveau mot de passe">
+                    <input type="text" class="form-control mb-3" name="img" id="img" placeholder="URL de l'image" 
+                    value="<?php echo $valuesInputed['img']; ?>">
+
+                    <select class="form-select mb-3" aria-label="Default select example" id="departementId" name="departementId">
+                        <?php 
+                            $selectedDepartmentId = $valuesInputed['departementId'];
+                        ?>
+                        <option value="-1" <?php echo ($selectedDepartmentId == -1) ? 'selected' : ''; ?>>Choisissez un département</option>
+
+                        <?php
+                        // Database connection
+                        $servername = "localhost";
+                        $usernameDB = "root";
+                        $passwordDB = "root";
+                        $db = "event_feedback";
+                        $conn = mysqli_connect($servername, $usernameDB, $passwordDB, $db);
+
+                        if (!$conn) {
+                            die("Connection failed: " . mysqli_connect_error());
+                        }
+                        $conn->query('SET NAMES utf8');
+
+                        // Query to fetch departments
+                        $query = "SELECT `id`, `name` FROM `departements`";
+                        $result = mysqli_query($conn, $query);
+
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                $selectedAttribute = ($selectedDepartmentId == $row["id"]) ? 'selected' : '';
+
+                                echo '<option value="' . $row["id"] . '" ' . $selectedAttribute . '>' . $row["name"] . '</option>';
+                            }
+                        }
+
+                        // Close the database connection
+                        mysqli_close($conn);
+                        ?>
+                    </select>
+
+                    <textarea class="form-control mb-3" name="description" id="description" 
+                    placeholder="Description" rows="4" style="max-height: 200px;" maxlength="500"><?php echo $valuesInputed['description']; ?></textarea>
 
                     <input type="hidden" id="hiddenId" name="hiddenId" value="<?php echo $eventId;?>">
 
@@ -157,12 +212,21 @@ if ($_SESSION["connexion"] == true) {
 
                     <div class="row">
                         <div class="col-6">
-                            <a href="users.php?id=<?php echo $eventId; ?>">
+                            <?php 
+                                $previousPage = $_GET['previousPage'];
+                                if ($previousPage == 'eventInfo') {
+                                    $cancelLink = "event-info.php?id=${eventId}";
+                                }
+                                else if ($previousPage == 'events') {
+                                    $cancelLink = 'events.php';
+                                }
+                            ?>
+                            <a href="<?php echo $cancelLink; ?>">
                                 <button type="button" class="btn btn-outline-danger w-100">Annuler</button>
                             </a>
                         </div>
                         <div class="col-6">
-                            <button type="submit" class="btn btn-primary w-100">Modifier</button>
+                            <button type="submit" class="btn btn-primary w-100">Sauvegarder</button>
                         </div>
                     </div>
                 </form>
